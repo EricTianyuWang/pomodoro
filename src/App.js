8ccc84f695acfba72d09f4cd481ca634f31eb094
Eric Tianyu Wang <ericliw@att.net>
2019-05-28 15:49:45 -0400

    Eric changed this

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 export default class App extends React.Component {
+
   render() {
     return(
       <div>test</div>
