b960cc75953adff9da06e1c4f5be7fb4f48a26ac
Anne Williams <mawilliams098@gmail.com>
2019-05-29 10:05:54 -0400

    added break separate break timer

--- src/Clock.js
+++ src/Clock.js
@@ -3,25 +3,11 @@ import Timer from "react-compound-timer";
 
 
 export class Clock extends Component {
-    constructor(props){
-        super(props);
-         this.state = {
-          seconds: '00',   // responsible for the seconds 
-          minutes: '25'  // responsible for the minutes
-       }
-    }
-
-    handlePomodoroClick = (e) => {
-        this.setState({minutes: 25});
-    }
-
-    handleBreakClick = (e) => {
-        this.setState({minutes: 5});
-    }
 
     render() {
         return (
             <div>
+            <h1> Pomodoro Timer </h1>
             <Timer
                 initialTime={1500000}
                 direction = "backward"
@@ -30,10 +16,9 @@ export class Clock extends Component {
                 {({ start, resume, pause, stop, reset, timerState }) => (
                     <React.Fragment>
                         <div>
-                            <Timer.Minutes /> minutes
+                            <Timer.Minutes /> minutes <br/>
                             <Timer.Seconds /> seconds
                         </div>
-                        <div>{timerState}</div>
                         <br />
                         <div>
                             <button onClick={start}>Start</button>
@@ -45,8 +30,30 @@ export class Clock extends Component {
                     </React.Fragment>
                 )}
             </Timer>
-                <button onClick={this.handlePomodoroClick}>Pomodoro</button>
-                <button onClick={this.handleBreakClick}>Break</button>
+
+            <h1> Break Timer </h1>
+            <Timer
+                initialTime={300000}
+                direction = "backward"
+                startImmediately={false}
+            >
+                {({ start, resume, pause, stop, reset, timerState }) => (
+                    <React.Fragment>
+                        <div>
+                            <Timer.Minutes /> minutes <br/>
+                            <Timer.Seconds /> seconds
+                        </div>
+                        <br />
+                        <div>
+                            <button onClick={start}>Start</button>
+                            <button onClick={pause}>Pause</button>
+                            <button onClick={resume}>Resume</button>
+                            <button onClick={stop}>Stop</button>
+                            <button onClick={reset}>Reset</button>
+                        </div>
+                    </React.Fragment>
+                )}
+            </Timer>
             </div>
         )
     }
