a109965951f5e1c66354a4879374f611d205814f
Eric Tianyu Wang <ericliw@att.net>
2019-05-29 14:36:48 -0400

    Designed the interface for clock

--- src/Clock.js
+++ src/Clock.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
 import Timer from "react-compound-timer";
-import { Row, Col, Button } from 'antd';
+import { Row, Col, Button, Progress } from 'antd';
 
 export class Clock extends Component {
 
+    state = {
+        seconds: 25
+    }
+
+    progress = (updatedTime) => {
+        this.setState({
+            seconds: updatedTime
+        })
+    }
     timer = (name, startTime) => {
         return (
             <div>
                 <h1><b> {name} </b></h1>
                     <Timer
-                        initialTime={startTime} //300000 1500000
+                        initialTime={startTime} // 1500000 300000
                         direction = "backward"
                         startImmediately={false}
                     >
-                        {({ resume, pause, reset, timerState }) => (
+                        {({ resume, pause, reset }) => (
                             <React.Fragment>
                                 <div>
-                                    <h2><Timer.Minutes/> minutes</h2>
-                                    <h2><Timer.Seconds/> seconds</h2>
-                                </div>
+                                <Row>
+                                    <Col span={6}><Progress type="circle" percent={<Timer.Minutes></Timer.Minutes>} format={percent => <Timer.Minutes/>} /></Col>
+                                    <Col span={6}><h1><b> minutes</b></h1></Col>
+                                    <Col span={6}><Progress type="circle" percent={<Timer.Seconds></Timer.Seconds>} format={percent => <Timer.Seconds/>} /></Col>
+                                    <Col span={6}><h1><b> seconds</b></h1></Col>
+                                </Row>
+                                <br/>
+                                </div> 
                                 <div>
-                                    <Button onClick={resume}>Start</Button>
-                                    <Button onClick={pause}>Pause</Button>
-                                    <Button onClick={reset}>Reset</Button>
+                                    <Button onClick={resume} size="large">Start</Button>
+                                    <Button onClick={pause} size="large">Pause</Button>
+                                    <Button onClick={reset} size="large">Reset</Button>
                                 </div>
                             </React.Fragment>
                         )}
