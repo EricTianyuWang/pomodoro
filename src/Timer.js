694aa0d79c471a17baff39235f3de591ce8154cb
Anne Williams <mawilliams098@gmail.com>
2019-05-29 17:05:16 -0400

    cleaned up timer jsx again

--- src/Timer.js
+++ src/Timer.js
@@ -4,19 +4,16 @@ import Form from './Form.js'
 
 class Timer extends React.Component {
     
-
-render(){
-    //console.log(this.state.user);
-    console.log(this.props.user)
-return(
-    <div> 
-        <Clock user = {this.props.user}/>
-        <Form user = {this.props.user}/>
-    </div>
-
-)
-}
-
+    render(){
+        //console.log(this.state.user);
+        console.log(this.props.user)
+        return(
+            <div> 
+                <Clock user = {this.props.user}/>
+                <Form user = {this.props.user}/>
+            </div>
+        )
+    }
 }
 
 export default Timer;
