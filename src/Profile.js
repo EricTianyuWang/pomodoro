77702f4f864fff59bfc6cd69e50ed7ed7ae5cbb9
Anne Williams <mawilliams098@gmail.com>
2019-05-29 10:59:04 -0400

    pseudocode for profile page

--- src/Profile.js
+++ src/Profile.js
@@ -4,7 +4,10 @@ export class Profile extends Component {
     render() {
         return (
             <div>
-                
+                <h1>Your Profile</h1>
+                {/* Display Welcome, current user's name? */}
+                {/* Display user information */}
+                {/* Display log of user's past logs  */}
             </div>
         )
     }
