fb570015fa484768dc94d1019d504013044ffc78
Anne Williams <mawilliams098@gmail.com>
2019-05-31 10:43:32 -0400

    put quotes api on break page and basic styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import React from 'react';
-import Form from './Form.js';
+// import Form from './Form.js';
 import './App.css';
 
 import Home from './Home.js'
-import Routing from './Routing.js'
+// import Routing from './Routing.js'
 
-import Clock from './Clock';
-// import Profile from './Profile';
-import Quote from './Quote.js'
+// import Clock from './Clock';
+// // import Profile from './Profile';
+// import Quote from './Quote.js'
 
 export default class App extends React.Component {
 
   render() {
     return(
 
-
       <div>
         <Home/>
-        <Quote/>
       </div>
 
     );
