c95277c014967372e7ee198acfb2058290c38d8a
Anne Williams <mawilliams098@gmail.com>
2019-05-31 09:37:37 -0400

    successfully cleared form upon submit

--- src/Form.js
+++ src/Form.js
@@ -30,6 +30,14 @@ export default class Form extends React.Component {
   
   handleClick = () => {
     this.sendToDatabase();
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.setState({
+      title: "", 
+      text: "",
+    });
   }
 
   sendToDatabase = async() => {
@@ -66,7 +74,7 @@ export default class Form extends React.Component {
       <div className = "form">
         {/* {this.props.user} */}
         <h1><b>What did you accomplish?</b></h1>
-        <Input.TextArea placeholder="Title" rows={1} onChange={this.handleTitleChange}/>
+        <Input.TextArea placeholder="Title" rows={1} onChange={this.handleTitleChange} value={this.state.title}/>
         <div style={{ margin: '20px 20px' }} />
         <Input.TextArea
           placeholder="Write here..."
@@ -74,6 +82,7 @@ export default class Form extends React.Component {
           size="small"
           autosize={{minRows: 8, maxRows: 12}}
           onChange={this.handleTextChange}
+          value = {this.state.text}
         />
         <p></p>
         <p></p>
